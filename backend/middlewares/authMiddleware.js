5214e460426842ffe5a234a85cfe0cba66172e2e
kavinash2j <killerapk66@gmail.com>
2025-09-12 20:10:22 +0530

    fix

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -15,7 +15,7 @@ const authMiddleware = async (req, res, next) => {
 
         // 2. If not in header, check cookies
         if (!token && req.cookies?.token) {
-            // console.log("token from cookie", req.cookies)
+            console.log("token from cookie", req.cookies)
             token = req.cookies.token;
         }
         // console.log("from middlewaer token = ", token);
